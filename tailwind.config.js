9c038de329f8e1dae3e3c4e6c94308ee4a7485d8
Taras Yurchyshyn <yts.prof@gmail.com>
2024-07-15 21:20:16 +0300

    base TailWind CSS project created

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./node_modules/tw-elements/js/**/*.js",
+  ],
+  theme: {
+    extend: {
+      colors:{
+
+      },
+    },
+  },
+
+}
